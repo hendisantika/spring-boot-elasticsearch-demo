31e2bc56fed0c8f0f803158a6cd8610d004442d2
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 09:16:23 +0700

    Add createBook in BookController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearchdemo/controller/BookController.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearchdemo/controller/BookController.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.springbootelasticsearchdemo.controller;
 
+import id.my.hendisantika.springbootelasticsearchdemo.exception.DuplicateIsbnException;
 import id.my.hendisantika.springbootelasticsearchdemo.model.Book;
 import id.my.hendisantika.springbootelasticsearchdemo.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +38,9 @@ public class BookController {
         return bookService.getAll();
     }
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping
+    public Book createBook(@Valid @RequestBody BookDto book) throws DuplicateIsbnException {
+        return bookService.create(BookDto.transform(book));
+    }
 }
