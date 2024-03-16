9e473f8fe714cb444951fb0c4b2fa900cefe3e31
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 09:08:20 +0700

    Add findByAuthorName & findByIsbn in BookRepository 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootelasticsearchdemo/repository/BookRepository.java
+++ src/main/java/id/my/hendisantika/springbootelasticsearchdemo/repository/BookRepository.java
@@ -4,6 +4,9 @@ import id.my.hendisantika.springbootelasticsearchdemo.model.Book;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-elasticsearch-demo
@@ -16,4 +19,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BookRepository extends ElasticsearchRepository<Book, String> {
+    List<Book> findByAuthorName(String authorName);
+
+    Optional<Book> findByIsbn(String isbn);
 }
