5a418496f360f7dbc115b802c1dce74545eb5ecb
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 09:22:40 +0700

    Modify SpringBootElasticsearchDemoApplicationTests🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springbootelasticsearchdemo/SpringBootElasticsearchDemoApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootelasticsearchdemo/SpringBootElasticsearchDemoApplicationTests.java
@@ -1,18 +1,40 @@
 package id.my.hendisantika.springbootelasticsearchdemo;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.springbootelasticsearchdemo.service.BookService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.Assert.assertTrue;
 
 @Testcontainers
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootElasticsearchDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Container
+    private static final ElasticsearchContainer elasticsearchContainer = new BookElasticsearchContainer();
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private ElasticsearchTemplate template;
+
+    @BeforeAll
+    static void setUp() {
+        elasticsearchContainer.start();
+    }
+
+    @BeforeEach
+    void testIsContainerRunning() {
+        assertTrue(elasticsearchContainer.isRunning());
+        recreateIndex();
     }
 
 }
