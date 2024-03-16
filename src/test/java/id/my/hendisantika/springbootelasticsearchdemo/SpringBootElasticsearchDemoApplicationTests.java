7db8d9bd7efdf99954e2021fa742f6c541747ee5
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 09:21:56 +0700

    Modify SpringBootElasticsearchDemoApplicationTests🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springbootelasticsearchdemo/SpringBootElasticsearchDemoApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootelasticsearchdemo/SpringBootElasticsearchDemoApplicationTests.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.springbootelasticsearchdemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootElasticsearchDemoApplicationTests {
 
     @Test
